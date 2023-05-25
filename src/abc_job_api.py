755ca590b542593c0dc6435c7a8d650a6ae4d58c
electr0n4ik <andreyshka3@gmail.com>
2023-05-25 11:47:00 +0300

    create: abc_job_api.py - abstract class

---
+++ src/abc_job_api.py
@@ -0,0 +1,27 @@
+from abc import ABC, abstractmethod
+import json
+from requests import *
+
+
+class JobApi(ABC):
+
+    @abstractmethod
+    def __init__(self):
+        pass
+
+    @abstractmethod
+    def __str__(self):
+        pass
+
+    @abstractmethod
+    def connect(self):
+        pass
+
+    @abstractmethod
+    def get_vacancies(self):
+        pass
+
+    @staticmethod
+    def printj(data_dict) -> None:
+        """Выводит словарь в json-подобном удобном формате с отступами (Для разработки)"""
+        print(json.dumps(data_dict, indent=2, ensure_ascii=False))
