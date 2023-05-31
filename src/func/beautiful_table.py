d7088cd9cb8bfde6d4fcb62a0f3103e1744abb37
electr0n4ik <andreyshka3@gmail.com>
2023-05-31 16:43:05 +0300

    revision print_prettyteble to user_unterface

--- src/func/beautiful_table.py
+++ src/func/beautiful_table.py
@@ -1,11 +1,19 @@
-from prettytable import *
-import json
+from prettytable import PrettyTable, ALL
 
 
 def print_prettytable(json_data):
     table = PrettyTable()
-    table.field_names = json_data["items"][0].keys()
+    # table.field_names = json_data["items"][0].keys()
+    table.field_names = ["id", "name", "area"]
+    # Установка горизонтальных линий
+    table.hrules = ALL
+
     for item in json_data["items"]:
-        table.add_row(item.values())
+        item_values = [
+            item.get('id', ''),
+            item.get('name', ''),
+            item.get('area', {}).get('name', '')
+        ]
+        table.add_row(item_values)
 
     print(table)
